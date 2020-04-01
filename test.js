3a3cac5d0ee465e4597971cc0e2e7a2e447291bd
Zuhair Shahzad <57069953+zuhair21993@users.noreply.github.com>
2020-04-01 11:51:57 -0400

    init

---
+++ test.js
@@ -0,0 +1,17 @@
+d3.csv("output/aqi_collapsible.csv").then(data => {
+  // console.log(data)
+
+  var states = data.map(d => d.State)
+  var countys = data.map(d => d.County)
+  var year = data.map(d => d.Year)
+  var co_days = data.map(d => d.Days_CO)
+  var no2_days = data.map(d => d.Days_NO2)
+  var so2_days = data.map(d => d.Days_SO2)
+  var ozone_days = data.map(d => d.Days_Ozone)
+  var pm2 = data.map(d => d.Days_PM2_5)
+  var pm10 = data.map(d => d.Days_PM10)
+  // console.log(pm10)
+  
+})
+
+
